cef2ce75b16fc68df8fd3654a5e69bf2d71257b0
Robert Ferney <rob@ferney.org>
2024-05-15 14:38:58 -0600

    First Commit

---
+++ lib/date/period.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "period/version"
+
+module Date
+  module Period
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/date/period/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Date
+  module Period
+    VERSION = "0.1.0"
+  end
+end
